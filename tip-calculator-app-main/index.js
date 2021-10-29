29e714e39d5cc33546a70e1616a75aece84696db
venuvivek-cheruku <89539450+venuvivek-cheruku@users.noreply.github.com>
2021-10-29 17:49:13 +0100

    Add files via upload

---
+++ tip-calculator-app-main/index.js
@@ -0,0 +1,140 @@
+/* ========================== § DOM ELEMENTS === */
+const billInput = document.getElementById("billAmount");
+const tipButton = document.getElementsByClassName("calculator__tip-percent-radio");
+const tipInput = document.getElementById("calculator__tip-percent--input");
+const peopleInput = document.getElementById("people");
+const tipTotalEl = document.getElementById("tipAmount");
+const tipPerPerson = document.getElementById("totalAmount");
+const resetButton = document.getElementById("reset-button");
+
+/* ========================== § RETRIEVE DATA FROM INPUTS === */
+function bill() {
+  return Number(billInput.value);
+}
+
+function tipPercent() {
+  if (checkedButtons()) {
+    const tipValue = Array.from(tipButton)
+      .filter((el) => el.checked)
+      .map((el) => el.getAttribute("data-percent"));
+    return Number(tipValue);
+  } else {
+    return Number(tipInput.value);
+  }
+}
+
+function people() {
+  return Number(peopleInput.value);
+}
+
+/* ========================== § CHECKS === */
+// are there buttons checked?
+function checkedButtons() {
+  return Array.from(tipButton).some((el) => el.checked);
+}
+
+// is there something written in the input fields?
+function somethingWritten(where) {
+  if (where === "everywhere") {
+    return tipPercent() > 0 && people() > 0 && bill() > 0;
+  } else if (where === "somewhere") {
+    return tipPercent() > 0 || people() > 0 || bill() > 0;
+  }
+}
+
+function errorMsg(element) {
+  return element.previousElementSibling;
+}
+
+// if the variable reset is set with the keyword reset, remove the class error, regardless of the content of the input, else add the class error if the input is falsy, remove it if it's truthy
+function validateInputs(reset) {
+  [billInput, peopleInput].forEach((el) => {
+    if (reset === "reset") {
+      errorMsg(el).classList.remove("error");
+      el.classList.remove("error");
+    } else {
+      if (el.value < 1 || el.value === "") {
+        errorMsg(el).classList.add("error");
+        el.classList.add("error");
+      } else if (el.value) {
+        errorMsg(el).classList.remove("error");
+        el.classList.remove("error");
+      }
+    }
+  });
+}
+
+/* ========================== § RESULTS === */
+function tipAmount() {
+  if (somethingWritten("everywhere")) {
+    return ((bill() / 100) * tipPercent()) / people();
+  } else {
+    return 0;
+  }
+}
+
+function totalPerPerson() {
+  return tipAmount() + bill();
+}
+
+/* ========================== § FUNCTIONALITY === */
+// make the reset button clickable when a button is selected or an input has a value
+function toggleResetButton() {
+  if (somethingWritten("somewhere") || checkedButtons()) {
+    resetButton.classList.remove("unclickable");
+    resetButton.classList.add("active");
+  } else {
+    resetButton.classList.add("unclickable");
+    resetButton.classList.remove("active");
+  }
+}
+
+// reset cancel all the contents of inputs and deselect buttons
+function reset() {
+  billInput.value = "";
+  peopleInput.value = "";
+  tipInput.value = "";
+  uncheckButtons();
+}
+
+// display the results
+function updateResults() {
+  tipTotalEl.innerText = tipAmount().toFixed(2);
+  tipPerPerson.innerText = totalPerPerson().toFixed(2);
+  toggleResetButton();
+  validateInputs();
+}
+
+// uncheck all buttons
+function uncheckButtons() {
+  for (let i = 0; i < tipButton.length; i++) {
+    tipButton[i].checked = false;
+  }
+}
+
+/* ========================== § EVENTS === */
+[billInput, peopleInput, tipInput].forEach((el) => {
+  el.addEventListener("keyup", function (e) {
+    return updateResults();
+  });
+
+  el.addEventListener("keydown", function (e) {
+    if (e.key.match(/\.|e/)) {
+      return e.preventDefault();
+    }
+  });
+});
+
+Array.from(tipButton).forEach((el) => {
+  el.addEventListener("click", function () {
+    return updateResults();
+  });
+});
+
+tipInput.addEventListener("click", function () {
+  uncheckButtons();
+});
+
+resetButton.addEventListener("click", function () {
+  return reset(), updateResults(), validateInputs("reset");
+});
